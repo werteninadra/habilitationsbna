82bc5617b0478087303aaea73a3b1c712cc42bad
nadra <nadrawertani22@gmail.com>
2025-08-18 18:16:22 +0100

    modifierautorisation cote simpleagent

--- foyer-management-frontend/src/app/habilitations/habilitations/habilitations.component.ts
+++ foyer-management-frontend/src/app/habilitations/habilitations/habilitations.component.ts
@@ -42,7 +42,7 @@ export class HabilitationsComponent implements OnInit {
         private keycloakService: KeycloakService
     
   ) {}
-  //z
+  //
   ngOnInit(): void {
     this.loadCurrentUser();
     this.loadUsers();
